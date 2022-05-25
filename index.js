5aad4e22443d2cd249031d38756e7feb82bd5643
yaminhossen <streatdot616@gmail.com>
2022-05-25 13:01:14 +0600

    create api for get all tools

--- index.js
+++ index.js
@@ -12,6 +12,25 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function run() {
+    try {
+        await client.connect();
+        const toolCollection = client.db('tools-manufacturer-site').collection('tools');
+
+        app.get('/tool', async (req, res) => {
+            const query = {};
+            const cursor = toolCollection.find(query);
+            const tools = await cursor.toArray();
+            res.send(tools);
+        })
+    }
+    finally {
+
+    }
+}
+
+run().catch(console.dir);
+
 app.get('/', (req, res) => {
     res.send('Hello from tools manufacturer company')
 })
