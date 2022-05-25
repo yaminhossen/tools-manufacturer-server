6ce9534cf84c585ee0b6453dcb52f41dd0ced4f8
yaminhossen <streatdot616@gmail.com>
2022-05-25 12:36:42 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express()
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2oa2k.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+app.get('/', (req, res) => {
+    res.send('Hello from tools manufacturer company')
+})
+
+app.listen(port, () => {
+    console.log(`Tools app listening on port ${port}`)
+})
